c98f79dd188b2a5de9c7e64c980215b9c2aa0448
nelfarna <elfarnadia@gmail.com>
2015-01-02 02:52:49 -0600

    Added justified and swipebox

---
+++ client/templates/layout/layout.js
@@ -0,0 +1,25 @@
+Template.layout.events({
+	'click .swipebox': function (e) {
+  	e.preventDefault();
+	$.swipebox({
+		useCSS : true, // false will force the use of jQuery for animations
+		useSVG : true, // false to force the use of png for buttons
+		initialIndexOnArray : 0, // which image index to init when a array is passed
+		hideCloseButtonOnMobile : false, // true will hide the close button on mobile devices
+		hideBarsDelay : 0, // delay before hiding bars on desktop
+		afterOpen: null, // called after opening
+		loopAtEnd: false // true will return to the first image after the last image i
+	});
+  }
+//  'click .fancybox': function(e, t){
+	//     link = $(e.currentTarget);
+	//     $(this).fancybox({
+	//              'autoScale': true,
+	//              'type':'iframe',
+	//              'height': (typeof link.data('height') == 'undefined' ? link.data('height') : 340),
+	//              'width': (typeof link.data('width') == 'undefined' ? link.data('width') : 560),
+	//              'href': link.attr('href')
+	//         }).click();
+	//     return false;
+ //  }
+});
--- client/templates/site/gallery/album.js
+++ client/templates/site/gallery/album.js
@@ -6,19 +6,9 @@ Template.album.rendered = function () {
 	  lastRow: 'nojustify',
 	  fixedHeight: false,
 	  captions: true,
-	  margins: 1,
-	  randomize: false,
-	  extension: /.[^.]+$/,
-	  refreshTime: 250,
-	  waitThumbnailsLoad: true,
-	  justifyThreshold: 0.35,
-	  cssAnimation: false,
-	  imagesAnimationDuration: 300
-	}).on('jg.complete', function (e) {
-	  // this callback runs after the gallery layout is created
-	}).on('jg.resize', function (e) {
-	  // this callback runs after the gallery is resized
-	}).on('jq.rowflush', function (e) {
-	  // this callback runs when a new row is ready
+	  margins: 3,
 	});
-}
+
+
+};
+
