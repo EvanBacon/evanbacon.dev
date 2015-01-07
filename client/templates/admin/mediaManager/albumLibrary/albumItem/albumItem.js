0184d36bfd49e04425ad28a670a0293a276fb235
nelfarna <elfarnadia@gmail.com>
2015-01-07 16:01:36 -0600

    Improved checkbox selection

--- client/templates/admin/mediaManager/albumLibrary/albumItem/albumItem.js
+++ client/templates/admin/mediaManager/albumLibrary/albumItem/albumItem.js
@@ -2,17 +2,33 @@ Template.albumItem.helpers({
   count: function () {
     return this.content.length;
   },
+  thumbId: function() {
+    return "thumb-" + this._id;
+  },
+  isAdminSide: function() {
+    return isAdmin() && Router.current().route.getName() === 'albumManager';
+  }
 });
 
 Template.albumItem.events({
-	// 'click .destroy': function () {
- //      if(confirm("Delete album?")) {
- //        delArr = [this._id];
- //        Albums.remove({_id: { $in: delArr }});
- //      }
- //  	},
-  
- //   }
+	  'change input[type=checkbox]': function(e) {
+      e.preventDefault();
+      var numChecked = SelectionAction.getCheckedCount();
+      if(!numChecked) numChecked = 0;
+        var checked = $('#' + this._id).prop('checked');
+        var thumbId = "#thumb-" + this._id;
+        if(checked) {
+          $(thumbId).addClass("selected");
+          SelectionAction.setCheckedCount(++numChecked);
+        } else {
+
+          $(thumbId).removeClass("selected");
+          if (numChecked > 0) 
+            SelectionAction.setCheckedCount(--numChecked);
+          else
+            SelectionAction.setCheckedCount(0);
+        }
+    },
     'click .album-link': function (e) {
         e.preventDefault();
         var curr = Router.current().route.getName(),
