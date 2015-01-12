fd56febe9ea3b3023c79f21f95a8797b4f0b65ab
nelfarna <elfarnadia@gmail.com>
2015-01-12 05:04:04 -0600

    Disabled add button if nothing typed

--- client/templates/admin/mediaManager/tags/tagManager.js
+++ client/templates/admin/mediaManager/tags/tagManager.js
@@ -14,16 +14,40 @@ Template.tagManager.helpers({
   }
 });
 
+var addTag = function (t) {
+  var tagName = Validation.trimInput(t.find("#searchTag").value);
+  if (Validation.isNotEmpty(tagName)) {
+    Meteor.call('addTag', tagName, this._id, function (err, result) {
+      if (err) console.log(err);
+    });
+    t.find("#searchTag").value = '';
+  }
+};
+
 Template.tagManager.events({
   'click .add-tag': function (e, t) {
-      var tagName = Validation.trimInput(t.find("#searchTag").value);
-      if (Validation.isNotEmpty(tagName)) {
-        Meteor.call('addTag', tagName, this._id, function (err, result) {
-          if (err) console.log(err);
-        });
-        t.find("#searchTag").value = '';
-      }
+      addTag(t);
+      // var tagName = Validation.trimInput(t.find("#searchTag").value);
+      // if (Validation.isNotEmpty(tagName)) {
+      //   Meteor.call('addTag', tagName, this._id, function (err, result) {
+      //     if (err) console.log(err);
+      //   });
+      //   t.find("#searchTag").value = '';
+      // }
   },
+  'keyup input#searchTag': function (e, t) {
+     //if (e.which === 13) 
+     if(t.find("#searchTag").value !== "") {
+      console.log('not empty');
+        $(".add-tag").removeClass("btn-default").removeClass("disabled").addClass("btn-danger");
+     } else {
+        $(".add-tag").removeClass("btn-danger").addClass("btn-default").addClass("disabled");
+     }
+   },
+   'submit form': function (e, t) {
+     e.preventDefault();
+     addTag(t);
+   },
 });
 
 Template.currentTags.events({
