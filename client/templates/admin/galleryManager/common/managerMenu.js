cf311455280a39ffe47f03f8c6b7b22b1f6e2e6e
nelfarna <elfarnadia@gmail.com>
2015-01-14 04:00:13 -0600

    Slight change in file structure

--- client/templates/admin/galleryManager/common/managerMenu.js
+++ client/templates/admin/galleryManager/common/managerMenu.js
@@ -1,4 +1,4 @@
-Template.managerHeader.helpers({
+Template.managerMenu.helpers({
 	isActive: function (type) {
 		var name = Router.current().route.getName();
 		return (name === type) ? 'active bold' : 'dim';
