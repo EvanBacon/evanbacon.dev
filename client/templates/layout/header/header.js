57c3594f2fefd8fd4982d51fad6640df4b7e8330
nelfarna <elfarnadia@gmail.com>
2015-02-01 04:04:47 -0600

    fixed sidebar toggle not closing

--- client/templates/layout/header/header.js
+++ client/templates/layout/header/header.js
@@ -1,19 +1,4 @@
 
-Template.header.rendered = function () {
-	$('[data-toggle=offcanvas]').click(function() {
-  		$(this).toggleClass('visible-xs text-left');
-    	$(this).find('i').toggleClass('glyphicon-align-justify glyphicon-chevron-left');
-	    $('.row-offcanvas').toggleClass('active');
-	    $('#lg-menu').toggleClass('hidden-xs').toggleClass('visible-xs');
-	    $('#album-list').toggleClass('hidden-xs').toggleClass('visible-xs');
-	    $('#xs-menu').toggleClass('hidden-xs').toggleClass('visible-xs');
-	    $('#sidebar-footer').toggleClass('hidden-xs').toggleClass('visible-xs');
-	    $('#logo').toggleClass('hidden-xs').toggleClass('visible-xs');
-	    $('#title').toggleClass('visible-xs').toggleClass('hidden-xs');
-	    $('#btnShow').toggle();
-    });
-};
-
 Template.header.helpers({
 	isActive: function (type) {
 		var name = Router.current().route.getName();
@@ -22,7 +7,16 @@ Template.header.helpers({
 });
 
 Template.header.events ({
-	'click #lg-menu a': function (e) {
-		$('.row-offcanvas').removeClass('active');
+	'click [data-toggle=offcanvas], click .active a': function (e) {
+		$('[data-toggle=offcanvas]').toggleClass('visible-xs text-left');
+    	$('[data-toggle=offcanvas]').find('i').toggleClass('glyph-menu glyphicon-chevron-left');
+	    $('.row-offcanvas').toggleClass('active');
+	    $('#lg-menu').toggleClass('active');
+	    $('#tag-list').toggleClass('active');
+	    $('#lg-menu').toggleClass('hidden-xs').toggleClass('visible-xs');
+	    $('#sidebar-footer').toggleClass('hidden-xs').toggleClass('visible-xs');
+	    $('#logo').toggleClass('hidden-xs').toggleClass('visible-xs');
+	    $('#tag-list').toggleClass('hidden-xs').toggleClass('visible-xs');
+	    $('#title').toggleClass('visible-xs').toggleClass('hidden-xs');
 	}
 });
