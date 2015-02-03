d039fdbaee2a1dd03c591e7bcba652d9fb867847
nelfarna <elfarnadia@gmail.com>
2015-02-02 21:18:53 -0600

    improved registration form and startup screens

--- client/templates/includes/siteName.js
+++ client/templates/includes/siteName.js
@@ -2,5 +2,13 @@ Template.siteName.helpers({
 	home: function () {
 		var name = Router.current().route.getName();
 		return name === 'home';
+	},
+	logoSet: function () {
+		var logo = getSetting('logoUrl');
+		return !! logo;
+	},
+	titleSet: function () {
+		var title = getSetting('title');
+		return !! title;
 	}
 });
--- server/fixtures.js
+++ server/fixtures.js
@@ -44,7 +44,7 @@ Meteor.startup( function() {
 
 		Settings.insert({
 			"aboutCode" : "<p class=\"text-center\">\nGo to <a href=\"/admin/settings\">settings</a> to add some content here.</p>\n",
-			"footerCode" : "<div class=\"home-links\">\n\t\t<a href=\"/albums\">Albums</a> / \n\t\t<a href=\"/about\">About</a> / \n\t\t<a href=\"/contact\">Contact</a> /\n<br>\n</div>",
+			"footerCode" : "<div class=\"home-links\">\n\t\t<a href=\"/albums\">Albums</a> / \n\t\t<a href=\"/about\">About</a> / \n\t\t<a href=\"/contact\">Contact</a>\n<br>\n</div>",
 			"homeDescription" : "Go to <a href=\"/admin/settings\">settings</a> to add some content here.</p>\n",
 			"imageMaxSize" : 500000,
 			"imageMaxWidth" : 1400,
