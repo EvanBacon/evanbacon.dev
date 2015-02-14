13f5da58435c5daca9d4459c7a70193612af38dd
nelfarna <elfarnadia@gmail.com>
2015-02-13 20:36:49 -0600

    changed how album samples are displayed, #samples from settings

--- client/templates/site/gallery/albumGroup.js
+++ client/templates/site/gallery/albumGroup.js
@@ -2,47 +2,50 @@
 // sampleCount = # random sample images from album
 // showTitle = true/false - show the album title box amidst samples
 
+Template.albumGroup.rendered = function() {
+	$('.itemP').hover(
+        function(){
+            $(this).find('.caption').fadeIn(300); 
+        },
+        function(){
+            $(this).find('.caption').fadeOut(300); 
+        }
+    );
+};
+
 Template.albumGroup.helpers({
 	samples: function () {
-		var featuredId = _.findWhere(this.content, { isFeatured: 1}).id;
-		if (typeof featuredId === undefined || ! featuredId)
-			featuredId = this.content[0].id;
-		// var media = Media.find({ $and: [{'metadata.albums._id': this._id},
-		// 								{ _id: {$ne: featured }} ]} ).fetch();
-		var media = Media.find({'metadata.albums._id': this._id}).fetch();
-
-		var featured = _.findWhere(media, { _id: featuredId });
-		var count = !! Template.parentData(1).sampleCount ? parseInt(Template.parentData(1).sampleCount) : 2;
-
-		var samples = _.sample(_.without(media, featured), count );
-
-		return _.union(samples, featured);	
-
+		return Media.find({'metadata.albums._id': this._id}, { limit: getSetting('numberSamplesFromAlbum', 2)}).fetch();
 	},
 	featured: function () {
-
 		var featured = _.findWhere(this.content, { isFeatured: 1});
 		if (typeof featured === undefined || ! featured)
 			featured = this.content[0];
-		return featured;
+		return Media.findOne({_id: featured.id});
 	},
 	count: function () {
 	    if (!! this.content)
 	      return this.content.length;
 	},
 	showTitle: function () {
-		return typeof Template.parentData(1).showTitle === undefined ? true : Template.parentData(1).showTitle;
+		return typeof Template.parentData(1).showTitles === undefined ? true : Template.parentData(1).showTitles;
 	}
 });
 
 Template.albumGroup.events({
-	'mouseover .album-link': function (e) {
-		$('#item-' + Template.parentData(0).slug ).addClass('album-selected');
+	'mouseover .item-album': function (e) {
+		$('#item-' + Template.parentData(0).slug ).addClass('album-selected').fadeIn();
 
 	},
-	'mouseout .album-link': function (e) {
+	'mouseout .item-album': function (e) {
 		$('#item-' + Template.parentData(0).slug ).removeClass('album-selected');
-	}
+	},
+	'click .album-link, click .album-title': function (e) {
+        e.preventDefault();
+        // resetAlbumGrid();
+        // initAlbumGrid();
+        Router.go( 'album', {slug: Template.parentData(0).slug});
+    },
 });
 
 
