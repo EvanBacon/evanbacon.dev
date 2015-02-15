48ea4651b0b469ed4a277e734a77cf28f7658f83
nelfarna <elfarnadia@gmail.com>
2015-02-15 17:31:45 -0600

    fixed fast-render issues

--- server/settings.js
+++ server/settings.js
@@ -1,15 +1,3 @@
 Meteor.publish('settings', function() {
 	return Settings.find({});
 });
-
-Settings.allow({
-	insert: function (userId) {
-		return isAdminById(userId);
-	},
-	update: function (userId) {
-		return isAdminById(userId);
-	},
-	remove: function (userId) {
-		return isAdminById(userId);
-	},
-});
