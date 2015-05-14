ae8d8b5dde53df99f52d4e8fe0b433274d790ebb
nelfarna <elfarnadia@gmail.com>
2015-05-13 19:05:19 -0500

    changed analytics package

--- packages/elmar-google-analytics/lib/routing.js
+++
@@ -1,6 +0,0 @@
-Router.onAfterAction( loadGoogleAnalytics );
-Router.onAfterAction( function() { 
-	if (typeof ga !== 'undefined'){
-		ga("send", "pageview", {'page': Router.current().location.get().path });
-    }
-});
