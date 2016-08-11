e474d26da7c3ede34603ab6bb60f4c2e4ca2fbc7
Evan Bacon <evan.bacon@frogdesign.com>
2016-08-11 16:39:40 -0500

    Init

---
+++ client/templates/layout/components/title/title.js
@@ -0,0 +1,16 @@
+Template.title.created = function() {
+  $(document).ready(function(){
+  var mouseX, mouseY;
+  var ww = $( window ).width();
+  var wh = $( window ).height();
+  var traX, traY;
+  $(document).mousemove(function(e){
+    mouseX = e.pageX;
+    mouseY = e.pageY;
+    traX = ((4 * mouseX) / 570) + 40;
+    traY = ((4 * mouseY) / 570) + 50;
+    // console.log(traX);
+    $(".main-title").css({"background-position": traX + "%" + traY + "%"});
+  });
+});
+};
--- client/templates/site/static/atelier.js
+++ client/templates/site/static/atelier.js
@@ -5,3 +5,55 @@ Template.atelier.helpers({
 		return Works.find({});
 	}
 });
+Template.atelier.rendered = function() {
+  $(document).ready(function() {
+        $('.material-card > .mc-btn-action').click(function () {
+
+						$('.material-card > .mc-btn-action').not(this).each(function(){
+							var card = $(this).parent('.material-card');
+							var icon = $(this).children('i');
+
+							if (card.hasClass('mc-active')) {
+									icon.addClass('fa-spin-fast');
+									card.removeClass('mc-active');
+
+									window.setTimeout(function() {
+											icon
+													.removeClass('fa-arrow-left')
+													.removeClass('fa-spin-fast')
+													.addClass('fa-bars');
+
+									}, 800);
+							}
+				      });
+
+            var card = $(this).parent('.material-card');
+
+            var icon = $(this).children('i');
+            icon.addClass('fa-spin-fast');
+
+            if (card.hasClass('mc-active')) {
+                card.removeClass('mc-active');
+
+                window.setTimeout(function() {
+                    icon
+                        .removeClass('fa-arrow-left')
+                        .removeClass('fa-spin-fast')
+                        .addClass('fa-bars');
+
+                }, 800);
+            } else {
+              console.log("other clicked");
+                card.addClass('mc-active');
+
+                window.setTimeout(function() {
+                    icon
+                        .removeClass('fa-bars')
+                        .removeClass('fa-spin-fast')
+                        .addClass('fa-arrow-left');
+
+                }, 800);
+            }
+        });
+    });
+};
