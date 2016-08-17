8a18781e1fc0f12a27b7950bb80451fec6136bb0
Evan Bacon <evan.bacon@frogdesign.com>
2016-08-16 20:55:37 -0500

    experience

--- client/templates/layout/components/cubes/cubes.js
+++ client/templates/layout/components/cubes/cubes.js
@@ -32,6 +32,22 @@ Template.works.rendered = function() {
         },
       });
 
+      $(".timeline-carousel").owlCarousel({
+        navigation : true, // Show next and prev buttons
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        autoPlay: 3000, //Set AutoPlay to 3 seconds
+        items : 1, // THIS IS IMPORTANT
+        responsive : {
+          480 : { items : 1  }, // from zero to 480 screen width 4 items
+          768 : { items : 2  }, // from 480 screen widthto 768 6 items
+          1024 : { items : 3   // from 768 screen width to 1024 8 items
+          }
+        },
+      });
+
+
+
       var gui, perlin;
 
       $(function() {
