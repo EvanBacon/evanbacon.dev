2deab59ba40a4b9adce94223d921b3d39ab4c5ba
Evan Bacon <baconbrix@gmail.com>
2020-01-14 22:08:29 -0800

    updated appearance switch

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,7 @@
 import { useActionSheet } from '@expo/react-native-action-sheet';
 import PropTypes from 'prop-types';
 import React from 'react';
-import { Linking, Platform, Text, View, Switch } from 'react-native';
+import { Linking, Platform, Animated, Text, View, Switch } from 'react-native';
 import { useDimensions, useREM } from 'react-native-web-hooks';
 import StyleSheet from 'react-native-extended-stylesheet';
 
@@ -135,8 +135,41 @@ const Header = ({ siteTitle }) => {
 
 function AppearanceSwitch() {
   const { isDark, setIsDark } = React.useContext(CustomAppearanceContext);
+  const value = React.useMemo(() => new Animated.Value(isDark ? 1 : 0), []);
 
-  return <Switch style={{ marginLeft: 12 }} value={isDark} onValueChange={(value) => setIsDark(value)} />
+  React.useEffect(() => {
+    Animated.timing(value).stop();
+    Animated.timing(value, {
+      duration: 150,
+      toValue: isDark ? 1 : 0
+    }).start();
+  }, [isDark]);
+
+  const tstyle = {
+    fontSize: 16,
+    textAlign: 'center',
+    marginHorizontal: 4,
+  };
+  return (
+    <View style={{ marginLeft: 12, alignItems: 'center', flexDirection: 'row' }}>
+      <Animated.Text style={[tstyle, { opacity: value.interpolate({ inputRange: [0, 1], outputRange: [1, 0] }), transform: [{translateX: value.interpolate({ inputRange: [0, 1], outputRange: [0, 20] }) }]  }]}>☀️</Animated.Text>
+      <Switch
+        tintColor="#4d4d4d"
+        onTintColor="#4d4d4d"
+        trackColor={{
+          false: '#4d4d4d',
+          true: '#4d4d4d'
+        }}
+        thumbTintColor="white"
+        thumbColor="white"
+        value={isDark}
+        onValueChange={(value) => {
+          setIsDark(value);
+        }} />
+      <Animated.Text style={[tstyle, { opacity: value.interpolate({ inputRange: [0, 1], outputRange: [0, 1] }), transform: [{translateX: value.interpolate({ inputRange: [0, 1], outputRange: [-20, 0] }) }] }]}>🌙</Animated.Text>
+
+    </View>
+  );
 }
 
 
