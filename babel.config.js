0eaac55d903b92f284409f8531c70a6e73337716
Evan Bacon <baconbrix@gmail.com>
2020-01-20 17:51:30 -0800

    fix native babel

--- babel.config.js
+++ babel.config.js
@@ -2,9 +2,6 @@
 // Learn more: https://github.com/expo/expo/blob/master/docs/pages/versions/unversioned/guides/using-nextjs.md#shared-steps
 
 module.exports = {
-    presets: ['@expo/next-adapter/babel'],
-    plugins: [
-        '@babel/plugin-proposal-class-properties',
-        '@babel/plugin-proposal-optional-chaining',
-    ],
+  presets: ['@expo/next-adapter/babel'],
+  plugins: ['@babel/plugin-proposal-optional-chaining'],
 };
