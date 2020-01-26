ec0bb99a4887ca861f61ed0ed4a9c14d323481c6
Evan Bacon <baconbrix@gmail.com>
2020-01-26 14:58:48 -0800

    Move meta to _app

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,25 +1,6 @@
 import { getInitialProps } from '@expo/next-adapter/document';
 import Document, { Head, Main, NextScript } from 'next/document';
 import React from 'react';
-import { Meta } from '../Data';
-
-const site = {
-  title: 'Evan Bacon',
-  author: '@baconbrix',
-  description:
-    'Learn more about Evan Bacon the Open Source Programmer and Master Lego Artist',
-};
-
-export function ensureSlash(inputPath: string, needsSlash: boolean): string {
-  const hasSlash = inputPath.startsWith('/');
-  if (hasSlash && !needsSlash) {
-    return inputPath.substr(1, inputPath.length);
-  } else if (!hasSlash && needsSlash) {
-    return `/${inputPath}`;
-  } else {
-    return inputPath;
-  }
-}
 
 class CustomDocument extends Document {
   static getInitialProps = async props => {
@@ -51,62 +32,7 @@ class CustomDocument extends Document {
 
   render() {
     // @ts-ignore
-    const { isProduction, dangerousAsPath } = this.props;
-    const themeColor = '#4630eb';
-
-    const currentPath = ensureSlash(dangerousAsPath, false) || 'talks';
-    const { image = {}, title = site.title, description = site.description } =
-      Meta[currentPath] || Meta.brand;
-
-    const injectMeta = [
-      {
-        name: `description`,
-        content: description,
-      },
-      // Open Graph
-      {
-        property: `og:description`,
-        content: description,
-      },
-      {
-        property: `og:title`,
-        content: title,
-      },
-      {
-        property: 'og:site_name',
-        content: 'Evan Bacon',
-      },
-      {
-        property: 'og:url',
-        content: `https://www.evanbacon.dev/${currentPath}`,
-      },
-      {
-        property: `og:type`,
-        content: `website`,
-      },
-      // Image
-      { property: 'og:image', content: `http://evanbacon.dev/${image.url}` },
-      {
-        property: 'og:image:secure_url',
-        content: `https://evanbacon.dev/${image.url}`,
-      },
-      { property: 'og:image:type', content: image.type },
-      { property: 'og:image:width', content: image.width },
-      { property: 'og:image:height', content: image.height },
-      { property: 'og:image:alt', content: image.description },
-      // Twitter
-      { name: `twitter:card`, content: `summary` },
-      { name: `twitter:creator`, content: site.author },
-      { name: `twitter:title`, content: title },
-      { name: `twitter:description`, content: description },
-      // Fix viewport by disabling scaling
-      {
-        key: 'viewport',
-        name: 'viewport',
-        content:
-          'width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1.00001,viewport-fit=cover',
-      },
-    ];
+    const { isProduction } = this.props;
 
     return (
       <html>
@@ -125,19 +51,6 @@ class CustomDocument extends Document {
             href="/apple-touch-icon.png"
           />
           <link rel="manifest" href="/site.webmanifest" />
-          <meta name="msapplication-TileColor" content={themeColor} />
-          <meta name="theme-color" content={themeColor} />
-          <meta
-            key="viewport"
-            name="viewport"
-            content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1.00001,viewport-fit=cover"
-          />
-
-          <title>{`${title} | ${site.title}`}</title>
-
-          {injectMeta.map((value, index) => {
-            return <meta key={`meta-${index}`} {...value} />;
-          })}
         </Head>
         <body>
           <Main />
