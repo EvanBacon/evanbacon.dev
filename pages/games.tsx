de53010100db9c7a7099ed670b6ad7a54fae633f
Evan Bacon <baconbrix@gmail.com>
2020-01-27 00:09:06 -0800

    Update games.tsx

--- pages/games.tsx
+++ pages/games.tsx
@@ -1,8 +1,16 @@
-import { LinearGradient } from 'expo-linear-gradient';
+import { Video } from 'expo-av';
 import React from 'react';
-import { Image, ImageBackground, StyleSheet, View } from 'react-native';
-import { useREM } from 'react-native-web-hooks';
+import {
+  Image,
+  ImageBackground,
+  StyleSheet,
+  View,
+  Platform,
+} from 'react-native';
+import { useHover, useREM, useLayout } from 'react-native-web-hooks';
+import isHoverEnabled from 'react-native-web-hooks/build/isHoverEnabled';
 
+import { BlurView } from 'expo-blur';
 import { H2, P } from '../components/Elements';
 import Layout from '../components/Layout';
 import PageHeader from '../components/PageHeader';
@@ -11,14 +19,84 @@ import UniversalLink from '../components/UniversalLink';
 import CustomAppearanceContext from '../context/CustomAppearanceContext';
 import { Projects } from '../Data';
 
+function MediaBackground({ resizeMode, isHovered, ...props }) {
+  const baseStyle = {
+    flex: 1,
+    minHeight: 360,
+    maxHeight: 360,
+  };
+
+  const videoRef = React.useRef(null);
+
+  React.useEffect(() => {
+    if (isHoverEnabled() && videoRef.current) {
+      if (isHovered) {
+        videoRef.current.playAsync();
+      } else {
+        videoRef.current.pauseAsync();
+      }
+    }
+  }, [isHovered]);
+
+  if (props.video) {
+    return (
+      <View style={baseStyle}>
+        <Video
+          ref={videoRef}
+          source={props.video}
+          posterSource={props.image}
+          isMuted
+          resizeMode={resizeMode}
+          shouldPlay={!isHoverEnabled()}
+          isLooping
+          style={StyleSheet.absoluteFill}
+        />
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            paddingHorizontal: 8,
+          }}
+        >
+          {props.children}
+        </View>
+      </View>
+    );
+  }
+
+  return (
+    <ImageBackground
+      source={props.image}
+      style={baseStyle}
+      resizeMode={resizeMode}
+    >
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          paddingHorizontal: 8,
+        }}
+      >
+        {props.children}
+      </View>
+    </ImageBackground>
+  );
+}
+
 function ProjectCard({
   title,
   icon,
   color,
   preview,
   url,
+  year,
   source,
+  group,
+  isDarkColored,
   description,
+  video,
 }) {
   const { isDark } = React.useContext(CustomAppearanceContext);
 
@@ -34,80 +112,162 @@ function ProjectCard({
   ];
 
   const themeColor = color || (isDark ? 'black' : 'white');
-  const ICON_SIZE = 96;
+  const ICON_SIZE = 72;
+
+  const ref = React.useRef(null);
+
+  const { isHovered } = useHover(ref);
+
+  const footerProps: any = { accessibilityRole: 'footer' };
+  const upperFlow = 16;
+  const { onLayout, width }: any = useLayout();
   return (
     <View
-      style={{
-        maxWidth: 720,
-        flex: 1,
-        marginBottom: 20,
-        backgroundColor: themeColor,
-      }}
+      onLayout={onLayout}
+      ref={ref}
+      style={[
+        {
+          maxWidth: 720,
+          flex: 1,
+          borderRadius: 12,
+          marginBottom: 20,
+          overflow: 'hidden',
+          shadowColor: 'black',
+          shadowRadius: 8,
+          shadowOpacity: 0.5,
+
+          shadowOffset: { height: 4, width: 0 },
+          backgroundColor: themeColor,
+          ...Platform.select({
+            web: {
+              transitionDuration: '0.4s',
+            },
+            default: {},
+          }),
+        },
+      ]}
     >
-      <ImageBackground
-        source={preview}
-        style={[
-          {
-            flex: 1,
-            paddingHorizontal: 8,
-            minHeight: 256,
-            justifyContent: 'center',
-            alignItems: 'center',
-          },
-        ]}
+      <MediaBackground
+        isHovered={isHovered}
+        image={preview}
+        video={video}
         resizeMode="cover"
       >
-        <LinearGradient
-          style={StyleSheet.absoluteFill}
-          colors={['rgba(0,0,0,0.5)', themeColor]}
-        />
-        <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-          <Image
-            source={icon}
-            style={{ width: ICON_SIZE, height: ICON_SIZE, borderRadius: 8 }}
-            resizeMode="cover"
-          />
-          {title && (
-            <H2
-              style={{
-                textAlign: 'center',
-                marginBottom: 0,
-                fontSize: useREM(1.51572),
-              }}
-            >
-              {title}
-            </H2>
-          )}
-          {description && <P>{description}</P>}
-        </View>
-      </ImageBackground>
-      <View style={{ flex: 1, paddingHorizontal: 40 }}>
         <View
+          {...footerProps}
           style={{
+            position: 'absolute',
+            bottom: 0,
+            justifyContent: 'space-between',
+            left: 0,
+            paddingHorizontal: 16,
+            paddingBottom: 12,
+            paddingTop: upperFlow + 8,
+            right: 0,
             flexDirection: 'row',
-            justifyContent: 'space-around',
-            marginTop: useREM(2.2),
+            alignItems: 'center',
+            maxWidth: '100%',
           }}
         >
-          {socials.map(social => (
-            <View key={social.name}>
-              <UniversalLink
-                style={{ marginRight: 8 }}
-                focusStyle={{ transform: [{ scale: 1.1 }] }}
-                target="_blank"
-                routeName={social.url}
-              >
-                <SocialIcon
-                  name={social.name}
-                  color={isDark ? 'white' : 'black'}
-                  size={16 * 2.2}
-                />
-              </UniversalLink>
-              <P style={{ marginTop: useREM(1.55) }}>{social.name}</P>
+          <View
+            style={{
+              backgroundColor: themeColor,
+              position: 'absolute',
+              opacity: 0.4,
+              top: upperFlow,
+              bottom: 0,
+              left: 0,
+              right: 0,
+            }}
+          />
+          <BlurView
+            intensity={100}
+            style={{
+              position: 'absolute',
+              top: upperFlow,
+              bottom: 0,
+              left: 0,
+              right: 0,
+            }}
+          />
+          <View
+            style={{ flexDirection: 'row', flexShrink: 1, paddingRight: 8 }}
+          >
+            <Image
+              source={icon}
+              style={{
+                width: ICON_SIZE,
+                height: ICON_SIZE,
+                marginRight: 16,
+                borderRadius: 8,
+                transform: [{ translateY: -upperFlow }],
+              }}
+              resizeMode="cover"
+            />
+            <View style={{ flex: 1 }}>
+              {title && (
+                <H2
+                  style={{
+                    color: 'white',
+
+                    marginBottom: 8,
+                    fontSize: useREM(1.51572),
+                    lineHeight: useREM(1.51572),
+                  }}
+                >
+                  {title}
+                </H2>
+              )}
+              {description && (
+                <P
+                  style={{
+                    color: 'white',
+                    marginBottom: 8,
+                  }}
+                >
+                  {description}
+                </P>
+              )}
+              {year && (
+                <P
+                  style={{
+                    color: 'white',
+                    fontWeight: 'bold',
+                    opacity: 0.7,
+                    marginBottom: 0,
+                  }}
+                >
+                  {year}
+                </P>
+              )}
             </View>
-          ))}
+          </View>
+
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-around',
+            }}
+          >
+            {socials.map((social, index) => (
+              <View key={social.name}>
+                <UniversalLink
+                  style={{ marginLeft: index === 0 ? 0 : 24 }}
+                  focusStyle={{ transform: [{ scale: 1.1 }] }}
+                  target="_blank"
+                  routeName={social.url}
+                >
+                  <SocialIcon
+                    name={social.name}
+                    color="white"
+                    size={16 * 2.2}
+                  />
+                </UniversalLink>
+              </View>
+            ))}
+          </View>
         </View>
-      </View>
+      </MediaBackground>
     </View>
   );
 }
