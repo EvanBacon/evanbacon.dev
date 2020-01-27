08ca3a567cfcce6a252838dd367536c12e384aa9
Evan Bacon <baconbrix@gmail.com>
2020-01-27 00:09:01 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 // Learn more: https://github.com/expo/expo/blob/master/docs/pages/versions/unversioned/guides/using-nextjs.md#withexpo
 
-const { withExpo } = require('@expo/next-adapter')
-const withFonts = require('next-fonts')
-const withImages = require('next-images')
-const withTM = require('next-transpile-modules')
-const withPlugins = require('next-compose-plugins')
+const { withExpo } = require('@expo/next-adapter');
+const withFonts = require('next-fonts');
+const withImages = require('next-images');
+const withTM = require('next-transpile-modules');
+const withPlugins = require('next-compose-plugins');
+const withVideos = require('next-videos');
 
 module.exports = withPlugins(
   [
@@ -16,9 +17,10 @@ module.exports = withPlugins(
     // ],
     withFonts,
     withImages,
+    withVideos,
     [withExpo, { projectRoot: __dirname }],
   ],
   {
     // ...
   }
-)
+);
