a76407003f21aa1c2f45b1365ba4cb69c929c9cd
Evan Bacon <baconbrix@gmail.com>
2020-03-23 12:36:00 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+/* eslint-env node */
 // Learn more: https://github.com/expo/expo/blob/master/docs/pages/versions/unversioned/guides/using-nextjs.md#withexpo
 
 const { withExpo } = require('@expo/next-adapter');
