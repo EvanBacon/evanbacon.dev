9a259b6c7ee52a561d3593fb87020a068692c2a9
Evan Bacon <baconbrix@gmail.com>
2020-06-12 14:39:08 -0700

    updated pillar valley link

--- Data.ts
+++ Data.ts
@@ -308,7 +308,7 @@ export const Projects: Project[] = [
     actions: [
       {
         icon: 'play',
-        url: 'https://evanbacon.github.io/Expo-Pillar-Valley/',
+        url: 'https://pillarvalley.netlify.app',
       },
       {
         icon: 'code',
