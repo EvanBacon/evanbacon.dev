e59100fef1bd2560c6d9ccd1f14ac2b6463cc374
evanbacon <bacon@expo.io>
2023-09-02 13:17:55 -0500

    drop more unused

--- src/components/AspectImage.tsx
+++
@@ -1,7 +0,0 @@
-import { Image } from 'react-native';
-
-import AspectRatio from './AspectRatio';
-
-const AspectImage = props => <AspectRatio Component={Image} {...props} />;
-
-export default AspectImage;
