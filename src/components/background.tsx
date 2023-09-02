f4af5e46a180d75a7d2a93bcbeec114ec4ebaeba
evanbacon <bacon@expo.io>
2023-09-02 14:01:32 -0500

    clean up background

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -1,9 +1,28 @@
 import styles from '@/styles/background.module.css';
+import classNames from 'classnames';
 
 export function Background() {
   return (
-    <div className={styles.main}>
-      <div className={styles.content} />
+    <div className="z-[-1] fixed w-full h-full pointer-events-none pt-24 pb-32 px-4 md:px-0 flex justify-center">
+      <div
+        className={classNames(
+          'absolute top-0 left-0 bottom-0 right-0',
+          'bg-[url("/grid.svg")] opacity-30'
+        )}
+      />
+      <div
+        className={classNames(
+          'flex-1 max-w-[640px]',
+          'opacity-10',
+          styles.complexGradient
+        )}
+      />
+      <div
+        className={classNames(
+          'absolute top-0 left-0 bottom-0 right-0',
+          styles.radialGradient
+        )}
+      />
     </div>
   );
 }
