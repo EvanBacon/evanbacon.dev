d38b5ca6643760b46109afe95590d377c4e34ddd
evanbacon <bacon@expo.io>
2023-09-03 12:54:11 -0500

    convert front images to avif

--- src/components/front/masonry.tsx
+++ src/components/front/masonry.tsx
@@ -2,47 +2,47 @@ import cn from 'classnames';
 import { Image } from 'react-native';
 
 const legoImages: [string, string][] = [
-  ['/front/lego/stanlee.jpg', 'Stan Lee and Evan Bacon in Austin, 2013'],
+  ['/front/lego/stanlee.avif', 'Stan Lee and Evan Bacon in Austin, 2013'],
 
   [
     '/front/lego/evan-superman.jpg',
     'Evan Bacon with his life-size Lego Superman statue in Houston, May 2013',
   ],
-  ['/front/lego/chrisevans.jpg', 'Chris Evans and Evan Bacon in Utah'],
+  ['/front/lego/chrisevans.avif', 'Chris Evans and Evan Bacon in Utah'],
   [
-    '/front/lego/batman.jpg',
+    '/front/lego/batman.avif',
     'Evan Bacon with his Lego Batman statue in Austin, August 2011',
   ],
   [
-    '/front/lego/kirk.jpg',
+    '/front/lego/kirk.avif',
     'Evan Bacon being interviewed about the construction of his life-size Lego Captain Kirk statue in Austin, 2013',
   ],
 ];
 const images: [string, string][] = [
   [
-    '/front/evan-europe.jpg',
+    '/front/evan-europe.avif',
     'Evan Bacon presenting Expo at React Europe, 2019',
   ],
   //   [
-  //     '/front/evan-lydia-vercel.jpg',
+  //     '/front/evan-lydia-vercel.avif',
   //     'Sebastian Markbåge, Addy Osmani, Lee Robinson, Evan Bacon, Lydia Hallie, JJ Kasper, Theo Browne, Sophie Alpert in San Francisco, 2023',
   //   ],
   [
-    '/front/evan-ReactiveConf.jpg',
+    '/front/evan-ReactiveConf.avif',
     'Evan Bacon discussing Progressive Web Apps at ReactiveConf in Prague, 2019',
   ],
 
   //   [
-  //     '/front/evan-appjs.jpg',
+  //     '/front/evan-appjs.avif',
   //     'Evan Bacon being interviewed about Expo Router v2 at App.js Conf in Kraków, 2023',
   //   ],
   //   [
-  //     '/front/evan_stan-lee.jpg',
+  //     '/front/evan_stan-lee.avif',
   //     'Evan Bacon explaining the internal structure of his life-size Lego Thor statue with Stan Lee, the creator of Thor, in Austin, 2013',
   //   ],
 
   [
-    '/front/evan_charlie.jpg',
+    '/front/evan_charlie.avif',
     'Evan Bacon and Charlie Cheever in Poland for App.js Conf 2023',
   ],
 
@@ -51,7 +51,7 @@ const images: [string, string][] = [
     'Addy Osmani, Evan Bacon, Lydia Hallie in San Francisco, 2022',
   ],
   [
-    '/front/evan-react.jpg',
+    '/front/evan-react.avif',
     'Ken Wheeler, Andrew Clark, Xuan Huang (黄玄), Evan Bacon, and Lydia Hallie in San Francisco 2022',
   ],
 ];
