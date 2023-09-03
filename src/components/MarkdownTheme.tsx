83dbd84b6e1f3ac3c843b5ffce6539c47aa0b0e5
evanbacon <bacon@expo.io>
2023-09-02 19:10:53 -0500

    Add quote block formatting

--- src/components/MarkdownTheme.tsx
+++ src/components/MarkdownTheme.tsx
@@ -21,6 +21,7 @@ import { Title } from '@/components/PostTitle';
 import { Prism, Highlight, themes } from 'prism-react-renderer';
 import cn from 'classnames';
 import { resolveAssetUri } from '@/utils/resolveMetroAsset';
+import Quote from './Quote';
 
 (typeof global !== 'undefined' ? global : window).Prism = Prism;
 
@@ -434,9 +435,26 @@ export function MarkdownTheme({ children }: { children: React.ReactNode }) {
           children,
           prevSibling,
           ...props
-        }) => (
-          <BlockQuote {...props} style={[style]}>
-            {/* <div
+        }) => {
+          const parsedChildren = React.Children.toArray(children);
+          const isQuote =
+            parsedChildren[0]?.props?.children?.[0] === '[!QUOTE]';
+
+          if (isQuote) {
+            const nextChildren = parsedChildren[0].props.children.slice(1);
+            let quote = nextChildren[0] as string;
+            const possibleAuthor = quote?.match(/~(?:\s+)?(.*)/m)?.[1]?.trim();
+
+            if (possibleAuthor) {
+              quote = quote.replace(/~(?:\s+)?(.*)/m, '').trim();
+            }
+            console.log('possibleAuthor', nextChildren[0], possibleAuthor);
+            return <Quote quote={quote} author={possibleAuthor} />;
+          }
+
+          return (
+            <BlockQuote {...props} style={[style]}>
+              {/* <div
               style={{
                 position: 'absolute',
                 top: 0,
@@ -457,9 +475,10 @@ export function MarkdownTheme({ children }: { children: React.ReactNode }) {
                 <path d="M21.6664 6.67319C19.8396 5.02559 17.4782 4.27661 15.0128 4.55796L15.0105 4.55823C11.0775 5.02532 7.941 8.22887 7.54886 12.1752C7.24603 15.0458 8.36328 17.8451 10.5543 19.6622C11.1657 20.1981 11.4917 20.8925 11.4917 21.6194V24.7615C11.4917 26.2694 12.7106 27.5 14.21 27.5H17.7953C19.2947 27.5 20.5136 26.2694 20.5136 24.7615V21.4243C20.5867 20.812 20.9006 20.1742 21.3775 19.7615C23.362 18.0925 24.5 15.6658 24.5 13.0966C24.5 10.6362 23.4678 8.29515 21.6664 6.67319ZM15.2667 6.69546C17.1231 6.47978 18.8863 7.04097 20.2428 8.26908C21.585 9.48256 22.3622 11.2458 22.3622 13.0883C22.3622 15.0198 21.5039 16.8532 20.0035 18.1106L19.9995 18.114C19.2208 18.7807 18.6947 19.7261 18.4748 20.6883H17.0757V16.9431C18.398 16.491 19.3431 15.219 19.3431 13.7433C19.3431 11.8804 17.8473 10.3665 16.0027 10.3665C14.1571 10.3665 12.6623 11.8897 12.6623 13.7433C12.6623 15.2273 13.608 16.4925 14.9297 16.9433V20.6966H13.5515C13.3445 19.6675 12.7874 18.7398 11.966 18.0214L11.9526 18.0078L11.9324 17.9912C10.298 16.6501 9.45341 14.5623 9.68934 12.3963L9.68988 12.3908C9.97813 9.4406 12.3251 7.04115 15.2667 6.69546ZM14.8082 13.7516C14.8082 13.0766 15.3516 12.5354 16.0027 12.5354C16.6537 12.5354 17.1971 13.0766 17.1971 13.7516C17.1971 14.4266 16.6537 14.9677 16.0027 14.9677C15.3516 14.9677 14.8082 14.4266 14.8082 13.7516ZM13.6377 24.7615V22.8573H18.3676V24.7615C18.3676 25.0833 18.1075 25.3393 17.7953 25.3393H14.21C13.8978 25.3393 13.6377 25.0833 13.6377 24.7615Z" />
               </svg>
             </div> */}
-            {children}
-          </BlockQuote>
-        )}
+              {children}
+            </BlockQuote>
+          );
+        }}
         img={Img}
         li={({
           firstChild,
--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -1,58 +1,36 @@
-import Laurel from '../../assets/laurel.svg';
 import { StyleSheet } from 'react-native';
 import { useREM } from 'react-native-web-hooks';
-import { BlockQuote, Div, Footer, P, Span } from '@expo/html-elements';
+
+import Laurel from '../../assets/laurel.svg';
 
 const color = 'white';
 
-export default function Quote({ quote, author, url }) {
+export default function Quote({
+  quote,
+  author,
+  url,
+}: {
+  quote?: any;
+  author?: string;
+  url?: string;
+}) {
   return (
-    <Div style={styles.container}>
+    <div className="flex flex-1 items-center justify-center flex-row py-4">
       <Laurel style={styles.startLeaf} />
-      <BlockQuote cite={url} style={styles.blockQuote}>
-        <Span style={styles.quote}>{quote}</Span>
-        <Footer>
-          <P style={styles.footerText}>{`~ ${author}`}</P>
-        </Footer>
-      </BlockQuote>
+      <blockquote cite={url} className="items-stretch flex  mx-2 flex-col">
+        <span className="text-white text-xl font-bold text-center font-serif">
+          {quote}
+        </span>
+        {author && (
+          <p className="text-white opacity-60 text-md italic text-center mt-2">{`— ${author} —`}</p>
+        )}
+      </blockquote>
       <Laurel style={styles.endLeaf} />
-    </Div>
+    </div>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    flexDirection: 'row',
-    transitionDuration: '200ms',
-    flex: 1,
-  },
-  blockQuote: {
-    alignItems: 'stretch',
-    flex: 1,
-    maxWidth: 720,
-    marginHorizontal: useREM(0.5),
-    transitionDuration: '200ms',
-  },
-  quote: {
-    color,
-    fontSize: 24,
-    flexWrap: 'wrap',
-    fontWeight: 'bold',
-    textAlign: 'left',
-    transitionDuration: '200ms',
-  },
-  footerText: {
-    color,
-    fontSize: 24,
-    fontStyle: 'italic',
-    textAlign: 'right',
-    fontWeight: 'bold',
-    transitionDuration: '200ms',
-    flex: 1,
-    marginBottom: 0,
-    marginTop: 15,
-  },
   startLeaf: { minWidth: 48, fill: color },
   endLeaf: { minWidth: 48, fill: color, transform: [{ scaleX: -1 }] },
 });
