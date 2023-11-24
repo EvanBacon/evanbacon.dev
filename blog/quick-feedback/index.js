c783e8ea009a7096fc0265229fbb79f627406351
evanbacon <bacon@expo.io>
2023-11-24 14:24:18 -0600

    wip

---
+++ blog/quick-feedback/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: 'Quick Action Feedback with Expo',
+  shortTitle: 'Quick Action Feedback with Expo',
+  subtitle:
+    'Collecting user feedback before app deletion with Quick Actions and Expo Router',
+  author: 'Evan Bacon',
+  date: '2020-02-19T22:45:36.616Z',
+  slug: 'html-elements',
+  tags: ['react', 'react-native', 'coding', 'development', 'javascript'],
+
+  // featuredImage: require('./images/header.webp'),
+};
