e7afd7702323be83b2f13001b81557b996720279
evanbacon <bacon@expo.io>
2024-01-04 11:05:32 +0100

    update

--- src/components/useIsFullScreenRoute.ts
+++ src/components/useIsFullScreenRoute.ts
@@ -3,5 +3,5 @@ import { usePathname } from 'expo-router';
 export function useIsFullScreenRoute() {
   const pathname = usePathname();
 
-  return pathname.match(/blog\/expo-2023/);
+  return pathname.match(/blog\/expo-2024/);
 }
